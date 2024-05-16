5e839db1e962f84e8ccb3a035de7f71b33056f4a
Vasilis Tsilidis <btsilidis@hotmail.com>
2024-05-16 20:01:58 +0300

    update CI.yml and MyLib.jl

--- src/MyLib.jl
+++ src/MyLib.jl
@@ -1,5 +1,27 @@
 module MyLib
 
-# Write your package code here.
+export func, my_print
+
+"""
+    func(x)
+
+Return double the number `x` plus `1`.
+"""
+func(x) = 2x + 1
+
+
+"""
+    my_print()
+
+Print "Hello world!".
+"""
+my_print() = println("Hello world!")
+
+"""
+    my_print2()
+
+Print "Hello world!!".
+"""
+my_print2() = println("Hello world!!")
 
 end
